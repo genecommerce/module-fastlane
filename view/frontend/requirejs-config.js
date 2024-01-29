e7bb1167793140fb371f2916ce14f818d958d263
Ivan Prysiazhniuk <prysiazhniuk.ivan.p@gmail.com>
2024-01-29 22:01:35 +0200

    changed braintree sdk version

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,19 @@
+var config = {
+    map: {
+        '*': {
+            braintree: 'https://js.braintreegateway.com/web/3.97.4-connect-alpha.6.3/js/client.min.js'
+        }
+    },
+
+    paths: {
+        'braintreePayPalCheckout': 'https://js.braintreegateway.com/web/3.97.4-connect-alpha.6.3/js/paypal-checkout.min',
+        'braintreeHostedFields': 'https://js.braintreegateway.com/web/3.97.4-connect-alpha.6.3/js/hosted-fields.min',
+        'braintreeDataCollector': 'https://js.braintreegateway.com/web/3.97.4-connect-alpha.6.3/js/data-collector.min',
+        'braintreeThreeDSecure': 'https://js.braintreegateway.com/web/3.97.4-connect-alpha.6.3/js/three-d-secure.min',
+        'braintreeApplePay': 'https://js.braintreegateway.com/web/3.97.4-connect-alpha.6.3/js/apple-pay.min',
+        'braintreeGooglePay': 'https://js.braintreegateway.com/web/3.97.4-connect-alpha.6.3/js/google-payment.min',
+        'braintreeVenmo': 'https://js.braintreegateway.com/web/3.97.4-connect-alpha.6.3/js/venmo.min',
+        'braintreeAch': 'https://js.braintreegateway.com/web/3.97.4-connect-alpha.6.3/js/us-bank-account.min',
+        'braintreeLpm': 'https://js.braintreegateway.com/web/3.97.4-connect-alpha.6.3/js/local-payment.min'
+    }
+};
