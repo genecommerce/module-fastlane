72767a5138f38ee2406fbc3ea66f9d0e846a52e9
Ivan Prysiazhniuk <prysiazhniuk.ivan.p@gmail.com>
2024-01-29 22:48:53 +0200

    fixed error from circleci

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -6,7 +6,8 @@ var config = {
     },
 
     paths: {
-        'braintreePayPalCheckout': 'https://js.braintreegateway.com/web/3.97.4-connect-alpha.6.3/js/paypal-checkout.min',
+        'braintreePayPalCheckout':
+            'https://js.braintreegateway.com/web/3.97.4-connect-alpha.6.3/js/paypal-checkout.min',
         'braintreeHostedFields': 'https://js.braintreegateway.com/web/3.97.4-connect-alpha.6.3/js/hosted-fields.min',
         'braintreeDataCollector': 'https://js.braintreegateway.com/web/3.97.4-connect-alpha.6.3/js/data-collector.min',
         'braintreeThreeDSecure': 'https://js.braintreegateway.com/web/3.97.4-connect-alpha.6.3/js/three-d-secure.min',
