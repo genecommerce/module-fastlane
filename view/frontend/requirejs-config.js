35151fca32d43fa731b60f1db03094184196e014
Max Parker <max.parker@gene.co.uk>
2024-02-21 11:05:13 +0000

    Fix issue where it was calling `checkoutConfig` on non checkout pages

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -7,8 +7,8 @@ var config = {
             'Magento_Checkout/js/view/shipping-information': {
                 'PayPal_Fastlane/js/view/shipping-information-mixin': true
             },
-            'PayPal_Braintree/js/view/payment/adapter': {
-                'PayPal_Fastlane/js/view/payment/adapter-mixin': true
+            'PayPal_Braintree/js/view/payment/method-renderer/cc-form': {
+                'PayPal_Fastlane/js/view/payment/method-renderer/cc-form-mixin': true
             },
             'PayPal_Braintree/js/view/payment/method-renderer/hosted-fields': {
                 'PayPal_Fastlane/js/view/payment/method-renderer/hosted-fields-mixin': true
